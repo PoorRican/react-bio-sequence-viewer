7ef2e12d406392a247f17cef6156d7e578f93d6f
Josue Figueroa <poor.rican@pm.me>
2022-09-02 16:17:19 -0700

    Create `Sequence` datatype

---
+++ src/types/sequence.js
@@ -0,0 +1,64 @@
+/**
+ * Array-like container for storing and manipulating sequences of monomers.
+ * Handles insertion, deletion, replacement, and rearrangement of segments.
+ * Provides an interface to store manipulations in an 'undo tree'.
+ */
+class Sequence extends Array {
+  /**
+   * Separate string into array
+   * @param sequence {string|String[]}
+   * @constructor
+   */
+  constructor(sequence) {
+    super(sequence);
+
+    if (typeof(sequence) === 'string') {
+      this.sequence = [...sequence];
+    } else {
+      this.sequence = sequence;
+    }
+  }
+
+  /**
+   * Insert given string at `index`
+   * @param value {string[]}
+   * @param index {number}
+   */
+  insert(value, index) {
+    this.sequence.splice(index, 0, ...value);
+  }
+
+  /**
+   * Delete given range
+   *
+   * @param range {[number, number]}
+   */
+  delete(range) {
+    this.sequence.splice(range[0], range[1] - range[0]);
+  }
+
+  /**
+   * Swap out given `range` with `insert`.
+   *
+   * Differs from `replace` in the sense that `replace` works in an element-wise manner.
+   *
+   * @param value {string[]} - Sequence to insert
+   * @param range {[number, number]} - Range to perform modification
+   */
+  swap(value, range) {
+    this.sequence.splice(range[0], range[1] - range[0], ...value);
+  }
+
+  /**
+   * Replace given `index` with `value`.
+   *
+   * Allows for creation of an SNP (Single Nucleotide Polymorphism)
+   *
+   * @param value {string} - Replacement value
+   * @param index {number} - Index to perform replacement
+   */
+  replace(value, index) {
+    if (value.length !== 1) Error('incorrect length of value');
+    this.sequence.splice(index, 1, value);
+  }
+}
