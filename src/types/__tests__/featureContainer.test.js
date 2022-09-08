487be1c2e1f972f65f14c9511f8db706912ab366
Josue Figueroa <poor.rican@pm.me>
2022-09-08 16:38:00 -0700

    Implement more `FeatureContainer` tests

--- src/types/__tests__/featureContainer.test.js
+++ src/types/__tests__/featureContainer.test.js
@@ -1,41 +1,63 @@
 import {Feature} from "../feature";
-import {FeatureContainer, generateFeatures} from "../featureContainer";
+import {FeatureContainer, generateFeatureStructure} from "../featureContainer";
 
 describe('Manipulation Functions', () => {
   test('add top-level feature', () => {
-    let fs = FeatureContainer.from(generateFeatures());
+    let fs = FeatureContainer.from(generateFeatureStructure());
     const feature = new Feature({id: 'new', location: [69, 71]});
 
-    let expected = new FeatureContainer(...fs);
+    let expected = FeatureContainer.from(generateFeatureStructure());
     expected.push(new Feature({...feature, accessor: 'new'}));
 
     expect(fs.add(feature)).toStrictEqual(expected);
   })
 
   test('add nested feature', () => {
-    let fs = FeatureContainer.from(generateFeatures());
-    const feature = new Feature({id: 'blah', location: [69, 71]})
+    let fs = FeatureContainer.from(generateFeatureStructure());
+    const feature = new Feature({id: 'new', location: [69, 71]})
 
-    let expected = new FeatureContainer(...fs);
-    expected[2].features = [new Feature({...feature, accessor: 'endBox::new'})];
+    let expected = FeatureContainer.from(generateFeatureStructure());
+    expected[1].features = [new Feature({...feature, accessor: 'endBox::new'})];
 
     expect(fs.add(feature, 'endBox')).toStrictEqual(expected);
   })
 
   test('delete top-level feature', () => {
-    let fs = FeatureContainer.from(generateFeatures());
+    let fs = FeatureContainer.from(generateFeatureStructure());
 
-    let expected = new FeatureContainer(...fs);
+    let expected = FeatureContainer.from(generateFeatureStructure());
     expected.splice(1,1);
 
     expect(fs.delete('endBox')).toStrictEqual(expected);
   })
 
   test('delete nested feature', () => {
+    let fs = FeatureContainer.from(generateFeatureStructure());
 
+    let expected = FeatureContainer.from(generateFeatureStructure());
+    expected[0].features.splice(0, 1);
+
+    expect(fs.delete('testFeature1::testFeature1_sub1')).toStrictEqual(expected);
   })
 
   test('edit feature', () => {
+    let fs = FeatureContainer.from(generateFeatureStructure());
 
+    let expected = FeatureContainer.from(generateFeatureStructure());
+    expected[2] = new Feature({...expected[2], location: [888,888]})
+
+    expect(fs.edit('markedIndex', {location: [888, 888]})).toStrictEqual(expected);
+  })
+})
+
+
+
+describe('Miscellaneous Functions', () => {
+  test('retrieve index', () => {
+    const fs = FeatureContainer.from(generateFeatureStructure());
+
+    const expected = [0, 0];
+
+    expect(fs.retrieve('testFeature1::testFeature1_sub1', true)).toStrictEqual(expected);
   })
 })
